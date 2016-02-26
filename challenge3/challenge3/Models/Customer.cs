6e8c04608ffa20fb0375db69b2b4da6df0e9597b
Derek Hobden <derek@prototype.ae>
2016-02-26 11:24:05 +0400

    Added models

---
+++ challenge3/challenge3/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace challenge3.Models
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+    }
+}
---
+++ challenge3/challenge3/Models/PhoneNumber.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace challenge3.Models
+{
+    public class PhoneNumber
+    {
+        public string Number { get; set; }
+        public bool Activated { get; set; }
+    }
+}

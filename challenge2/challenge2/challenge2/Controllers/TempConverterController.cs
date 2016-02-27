d0fa149a057ccc1222745433a2adfad33c1591e3
Derek Hobden <derek@prototype.ae>
2016-02-27 21:45:22 +0400

    Added Api to support asyncronous calls

---
+++ challenge2/challenge2/challenge2/Controllers/TempConverterController.cs
@@ -0,0 +1,19 @@
+﻿using challenge2.ServiceTempConverter;
+using System;
+using System.Web.Http;
+
+namespace challenge2.Controllers
+{
+    public class TempConverterController : ApiController
+    {
+        ConvertTemperatureSoapClient _tempConverter = new ConvertTemperatureSoapClient();
+
+        // GET: api/TempConverter/5
+        public double Get(double temperature, string fromUnit, string toUnit)
+        {
+            TemperatureUnit from = (TemperatureUnit)Enum.Parse(typeof(TemperatureUnit), fromUnit);
+            TemperatureUnit to = (TemperatureUnit)Enum.Parse(typeof(TemperatureUnit), toUnit);
+            return _tempConverter.ConvertTemp(temperature, from, to);
+        }
+    }
+}
